1414e112f0eb7ce976d5827d19cadd1101b59188
Jeffery-Frazier-Jr <jeffery.frazier.jr@gmail.com>
2024-09-09 21:54:46 -0400

    Layout and output setup

--- calculator.py
+++ calculator.py
@@ -11,7 +11,32 @@ class Calculator(ctk.CTk):
         self.geometry(f'{APP_SIZE[0]}x{APP_SIZE[1]}')
         self.resizable(False, False)                               # Disables resizing and sets window size based on APP_SIZE from settings
 
+        # grid layout
+        self.rowconfigure(list(range(MAIN_ROWS)), weight = 1, uniform = 'a')
+        self.columnconfigure(list(range(MAIN_COLUMNS)), weight = 1, uniform = 'a')
+
+        # data
+        self.formula_string = ctk.StringVar(value = '')
+        self.result_string = ctk.StringVar(value = '0')
+
+        # widgets
+        self.create_widgets()
+
         self.mainloop()
+
+    def create_widgets(self):
+        # fonts
+        main_font = ctk.CTkFont(family = FONT, size = NORMAL_FONT_SIZE)
+        result_font = ctk.CTkFont(family = FONT, size = OUTPUT_FONT_SIZE)
+
+        # output labels
+        OutputLabel(self, 0, 'se', main_font, self.formula_string) # formula
+        OutputLabel(self, 1, 'e', result_font, self.result_string) # result
+
+class OutputLabel(ctk.CTkLabel):
+    def __init__(self, parent, row, anchor, font, string_var):
+        super().__init__(parent, font = font, textvariable = string_var)
+        self.grid(column = 0, columnspan = 4, row = row, sticky = anchor, padx = 10)
 
 if __name__ == '__main__':
     Calculator(DARK_MODE)
